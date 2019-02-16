41020932e7b8bac0da55d120a2f596e3111009aa
Andrew <ak4jd@virginia.edu>
2019-02-16 16:35:13 -0500

    first commit

---
+++ components/HomeScreen.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { View, StyleSheet, Image, Text, Button } from 'react-native'
+
+export default class HomeScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <View style={styles.container}>
+                <Image source={require('../assets/Illustration.png')} />
+                <Text style={styles.titleText}>DinDin</Text>
+                <Text style={styles.subText}>Connecting Food Lovers</Text>
+                </View>
+                <Image style = {styles.getStartedButton} source={require('../assets/getStarted.png')} />
+            </View>
+
+        )
+    }
+}
+
+const styles = StyleSheet.create(
+    {
+        container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        titleText: {
+            fontSize: 28,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: 50,
+        },
+        subText: {
+            fontSize: 15,
+            fontStyle: 'italic',
+            color: 'grey',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        getStartedButton: {
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginBottom: 20,
+        },
+    }
+)

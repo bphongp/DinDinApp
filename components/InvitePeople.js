af416ad0573fd35a2b46fd6f024fb701b50b51ea
bphongp <bp8qg@virginia.edu>
2019-04-16 09:59:03 -0400

    trying to get invitepeople screen

--- components/InvitePeople.js
+++ components/InvitePeople.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { Constants } from 'expo';
-import { ListItem, CheckBox } from 'react-native-elements';
+import * as React from 'react'
+import { StyleSheet, Text, View, Image, FlatList, Button, TouchableOpacity, ScrollView} from 'react-native'
+import InvitationCard from './InvitationCard';
 import firebase from 'firebase';
 
 const firebaseConfig = {
@@ -10,58 +9,136 @@ const firebaseConfig = {
     databaseURL: "https://dindin-46b55.firebaseio.com",
     projectId: "dindin-46b55",
     storageBucket: "dindin-46b55.appspot.com",
-    messagingSenderId: "36010701085", 
+    messagingSenderId: "36010701085"
 };
 
-export default class App extends Component {
+
+export default class InvitePeople extends React.Component {
+
     constructor(props) {
         super(props)
         this.state = {
             eventsDataAll: [],
             eventsData: [],
             hasData: false,
-            invitesData: [],
-            inviteKey:null,
-            hasInviteData: false,
-            checked:false,
         }
     }
-    press = () => {
-        this.setState((state) => ({
-        checked: !state.checked,
-        }));
+   
+    componentWillMount() {
+        if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+        }
+        this.readUserData()
+        
     }
-    render() {
+
+  
+    readUserData() {
+       //console.log("readUserData Fired")
+
+        this.database = firebase.database();
+        firebase.database().ref('events/0001').on("value", snapshot => {
+            this.setState({ eventsData: Object.values(snapshot.val()), hasData: true, eventsDataAll: Object.values(snapshot.val())})
+           
+        })
+        console.log("events:"+this.state.eventsDataAll)
+
+        
+    }
+
+    renderRow({ item }) {
         return (
-        <View style={styles.container}>
-            <ListItem
-                title={
-                <CheckBox
-                    title="Click Here!"
-                    onPress={this.press}
-                    checked={this.state.checked}
-                />
-                }
+            
+            <View style={styles.card}>
+                <View style={{flex:1,flexDirection:'row'}}>
+                    <Image style={styles.image} source={{ uri: item.photo }} />
+                    <View style={{ marginTop: '3%', flex: 2 }}>
+                        <Text style={styles.text}>{item.name}</Text>
+                        <Text style={styles.text}>{ item.date.month + "/" + item.date.day + " " + item.date.time}</Text>
+                    </View>
+                    <View style={{ flex: .75, flexDirection: 'row'}}>
+
+                    </View>
+                </View>
+            </View>
+
+        )
+    }
+    static navigationOptions = {
+        title: "DinDin",
+        headerTitleStyle: {
+            fontSize: 20,
+            fontWeight: undefined,
+            alignSelf: 'center',
+            flexGrow: 1,
+            textAlign: 'center',
+        },
+        headerStyle: {
+            paddingVertical: 15,
+        },
+        headerRight: (
+            <View style={{
+                margin: 5,
+                paddingHorizontal: 10,
+            }}>
+                <Image source={require('../assets/search.png')} />
+            </View>
+        )
+
+    };
+    renderSeparator = () => {
+        return (
+            <View
+                style={{
+                    height: 1,
+                    width: "90%",
+                    backgroundColor: "#CED0CE",
+                    marginLeft: "5%"
+                }}
             />
-                    <ListItem
-                title={
-                <CheckBox
-                    title="Click Here!"
-                    onPress={this.press}
-                    checked={this.state.checked}
-                />
-                }
-            />
-        </View>
         );
+    };
+
+    render() {
+            return (
+                <View style = {{ flex: 1,}}>
+                </View>
+            )
+       
     }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-  },
-});
+const styles = StyleSheet.create(
+    {
+        card: {
+            overflow: 'hidden',
+            backgroundColor: 'white',
+            margin: 15,
+            height: 75,
+
+            //borderWidth: 1,
+            //borderColor: 'lightgrey',
+            //borderRadius: 8,
+        },
+        topContainer: {
+            flex: 1,
+            flexDirection: 'row',
+        },
+        image: {
+            width: 60,
+            height: 60,
+            borderRadius: 50,
+            margin: 10,
+            marginLeft: 15,
+        },
+        text: {
+            marginTop: 5,
+            marginLeft: 10
+        },
+        button: {
+            marginTop: 10,
+            borderRadius: 8,
+        },
+
+    }
+)
